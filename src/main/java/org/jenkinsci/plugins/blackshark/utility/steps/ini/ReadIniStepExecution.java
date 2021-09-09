07099d6536a72ea72adbbde68a0b80bedb949375
bright.ma <bright.ma@blackshark.com>
2021-09-09 11:22:41 +0800

    src: java: add try to read ini file from workspce child

--- src/main/java/org/jenkinsci/plugins/blackshark/utility/steps/ini/ReadIniStepExecution.java
+++ src/main/java/org/jenkinsci/plugins/blackshark/utility/steps/ini/ReadIniStepExecution.java
@@ -1,10 +1,15 @@
 package org.jenkinsci.plugins.blackshark.utility.steps.ini;
 
+import hudson.FilePath;
 import org.apache.commons.configuration.HierarchicalINIConfiguration;
+import org.jenkinsci.plugins.workflow.steps.MissingContextVariableException;
 import org.jenkinsci.plugins.workflow.steps.StepContext;
 import org.jenkinsci.plugins.workflow.steps.SynchronousNonBlockingStepExecution;
 
 import javax.annotation.Nonnull;
+import java.io.InputStream;
+
+import static org.apache.commons.lang.StringUtils.isNotBlank;
 
 public class ReadIniStepExecution extends SynchronousNonBlockingStepExecution<String> {
     private static final long serialVersionUID = 1L;
@@ -18,16 +23,25 @@ public class ReadIniStepExecution extends SynchronousNonBlockingStepExecution<St
 
     @Override
     protected String run() throws Exception {
-        String file = step.getFile();
-        String section = step.getSection();
-        String option = step.getOption();
-        try{
-            HierarchicalINIConfiguration ini = new HierarchicalINIConfiguration(file);
-            String value = ini.getSection(section).getString(option);
-            return value;
-        }catch (Exception e){
-            return "";
+        FilePath ws = getContext().get(FilePath.class);
+        if (ws == null && isNotBlank(step.getFile())) {
+            throw new MissingContextVariableException(FilePath.class);
         }
+
+        FilePath f = ws.child(step.getFile()); //
+        if (f.exists() && !f.isDirectory()) {
+            try (InputStream is = f.read()) {
+                HierarchicalINIConfiguration ini = new HierarchicalINIConfiguration();
+                ini.load(is);
+                String section = step.getSection();
+                String option = step.getOption();
+                String value = "";
+                value = ini.getSection(section).getString(option);
+                return value;
+            }
+        }
+        return "";
+
 
     }
 
